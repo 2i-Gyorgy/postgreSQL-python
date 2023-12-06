ba73631959bb4c9715cdfbe7a14a3ec94b04985a
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2023-12-06 11:22:59 +0000

    added table and data generator... much improvement needed

--- supabase_database/pyPostgresTest copy.py
+++ supabase_database/pyPostgresTest copy.py
@@ -1,26 +1,23 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
-import random
-import datetime
 
 load_dotenv()
 
-conn = psycopg2.connect(database = os.getenv("DATABASE_DATABASE"),
-                        user = os.getenv("DATABASE_USER"),
+conn = psycopg2.connect(database = "postgres",
+                        user = "postgres",
                         host = os.getenv("DATABASE_URL"),
                         password = os.getenv("DATABASE_PASSWORD"),
-                        port = os.getenv("DATABASE_PORT"))
+                        port = 5432)
 
 def create_table():
     # Open a cursor to perform database operations
     cur = conn.cursor()
     # Execute a command: create test_table table
-    cur.execute("""CREATE TABLE grafana_table(
+    cur.execute("""CREATE TABLE test_table(
                 test_id SERIAL PRIMARY KEY,
-                test_time TIMESTAMP(0),
                 test_name VARCHAR (50) UNIQUE NOT NULL,
-                test_outcome BOOLEAN);
+                test_more_property VARCHAR (100) NOT NULL);
                 """)
     # Make the changes to the database persistent
     conn.commit()
@@ -30,13 +27,11 @@ def create_table():
 
 def fill_data_into_table():
     cur = conn.cursor()
-    for i in range (256):
-        # Generate time - unix epoch
-        time = str(datetime.datetime.fromtimestamp(1701857824 + (i * 86400)))
-        name = 'test_case' + str(i)
-        outcome = str(random.getrandbits(1))
-        # print ('("INSERT INTO test_table(test_time, test_name, test_outcome) VALUES("' + time + '", '" + name + "', "' + outcome + '")");')
-        cur.execute("INSERT INTO grafana_table(test_time, test_name, test_outcome) VALUES('" + time + "', '" + name + "', '" + outcome + "')");
+    cur.execute("INSERT INTO test_table(test_name, test_more_property) VALUES('a','bc')");
+    cur.execute("INSERT INTO test_table(test_name, test_more_property) VALUES('b','cd')");
+    cur.execute("INSERT INTO test_table(test_name, test_more_property) VALUES('c','de')");
+    cur.execute("INSERT INTO test_table(test_name, test_more_property) VALUES('d','ef')");
+    cur.execute("INSERT INTO test_table(test_name, test_more_property) VALUES('e','fg')");
     conn.commit()
     cur.close()
     conn.close()
@@ -52,5 +47,5 @@ def select_all_from_table():
 
 
 # create_table()
-# fill_data_into_table()
+fill_data_into_table()
 # select_all_from_table()
