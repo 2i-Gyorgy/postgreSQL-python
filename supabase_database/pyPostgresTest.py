b69965a1c1efe64efcc3989df599c4265abc0381
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2023-12-11 08:42:52 +0000

    upate

--- supabase_database/pyPostgresTest.py
+++ supabase_database/pyPostgresTest.py
@@ -6,51 +6,69 @@ import datetime
 
 load_dotenv()
 
-conn = psycopg2.connect(database = os.getenv("DATABASE_DATABASE"),
+connection = psycopg2.connect(database = os.getenv("DATABASE_DATABASE"),
                         user = os.getenv("DATABASE_USER"),
                         host = os.getenv("DATABASE_URL"),
                         password = os.getenv("DATABASE_PASSWORD"),
                         port = os.getenv("DATABASE_PORT"))
 
+def table_exists():
+    """
+    Check if a table exists in the PostgreSQL database.
+
+    :param table_name: Name of the table to check.
+    :param connection: PostgreSQL database connection.
+    :return: True if the table exists, False otherwise.
+    """
+
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT EXISTS (
+                    SELECT 1 FROM information_schema.tables
+                    WHERE table_name = %s);
+                    """, ('grafana_table',))
+        exists = cursor.fetchone()[0]
+    return exists
+
 def create_table():
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
     # Execute a command: create test_table table
-    cur.execute("""CREATE TABLE grafana_table(
+    cursor.execute("""CREATE TABLE grafana_table(
                 test_id SERIAL PRIMARY KEY,
                 test_time TIMESTAMP(0),
                 test_name VARCHAR (50) UNIQUE NOT NULL,
                 test_outcome BOOLEAN);
                 """)
     # Make the changes to the database persistent
-    conn.commit()
-    # Close cursor and communication with the database
-    cur.close()
-    conn.close()
+    connection.commit()
+    print("'grafana_table' and schema created")
 
 def fill_data_into_table():
-    cur = conn.cursor()
     for i in range (256):
         # Generate time - unix epoch
         time = str(datetime.datetime.fromtimestamp(1701857824 + (i * 86400)))
         name = 'test_case' + str(i)
         outcome = str(random.getrandbits(1))
         # print ('("INSERT INTO test_table(test_time, test_name, test_outcome) VALUES("' + time + '", '" + name + "', "' + outcome + '")");')
-        cur.execute("INSERT INTO grafana_table(test_time, test_name, test_outcome) VALUES('" + time + "', '" + name + "', '" + outcome + "')");
-    conn.commit()
-    cur.close()
-    conn.close()
+        cursor.execute("INSERT INTO grafana_table(test_time, test_name, test_outcome) VALUES('" + time + "', '" + name + "', '" + outcome + "');")
+    # Make the changes to the database persistent
+    connection.commit()
 
 def select_all_from_table():
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM test_table;')
-    rows = cur.fetchall()
-    conn.commit()
-    conn.close()
+    cursor.execute('SELECT * FROM grafana_table;')
+    rows = cursor.fetchall()
+    connection.commit()
     for row in rows:
         print(row)
 
-
-# create_table()
+# Open a cursor to perform database operations
+cursor = connection.cursor()
+if not table_exists():
+    print("not found 'grafana_table', attempt to create it")
+    create_table()
+else:
+    print("'grafana_table' already present")
 # fill_data_into_table()
-# select_all_from_table()
+select_all_from_table()
+
+# Close cursor and communication with the database
+cursor.close()
+connection.close()
